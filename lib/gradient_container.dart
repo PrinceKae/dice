497f4218be9a118d3f1b36378bc52b6f127bece3
princekae <pkebayzim@gmail.com>
2023-05-31 13:01:05 +0100

    4th Commit

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -4,33 +4,53 @@ const startAlignment =  Alignment.topLeft;
 const endAlignment =   Alignment.bottomRight;
 
 
- class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color1, this.color2,{super.key,});
+ class GradientContainer extends StatefulWidget {
+   const GradientContainer(this.color1, this.color2,{super.key,});
+
 final Color color1;
 final Color color2;
 
+  @override
+  State<GradientContainer> createState() => _GradientContainerState();
+}
+
+class _GradientContainerState extends State<GradientContainer> {
+var activeDiceImage = 'assets/images/dice-4.png';
+
 void rollDice (){
+  activeDiceImage = 'assets/images/dice-6.png';
 
 }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration:  BoxDecoration(
         gradient: LinearGradient(
-          colors: [color1, color2],
+          colors: [widget.color1, widget.color2],
           begin:startAlignment,
           end: endAlignment,
         ),
       ),
       child: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize:MainAxisSize.min ,
           children: [
             Image.asset(
-              'assets/images/dice-6.png',
+              activeDiceImage,
               width: 200,
             ),
-            TextButton(onPressed: rollDice, child: Text('Roll Dice'))
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+                onPressed: rollDice,
+                style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    textStyle: const TextStyle(
+                      fontSize: 24,
+                    )),
+                child: const Text('Roll Dice'))
           ],
         ),
       ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,7 +14,7 @@ import 'package:floppy/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(GradientContainer());
+    await tester.pumpWidget(const GradientContainer());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
